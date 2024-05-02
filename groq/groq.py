f47f985bc9420472799e5cbde72a9dae07c5b46e
AvinashPolisetty <polisettyavinash2@gmail.com>
2024-05-02 17:49:04 +0530

    cassandra added

--- groq/groq.py
+++ groq/groq.py
@@ -11,9 +11,9 @@ import bs4
 load_dotenv()
 
 #database initialization
-groq_api_key=os.environ['GROQ_API_KEY']
-astra_db_app_token= "AstraCS:FLAsxRtrLzbdyRMbRhayEMXT:a9bb94b186ff03b2e1c67f2368de26a372025f39bdf130002aeb772bc0608aeb"
-astra_db_id="8e00d55c-6c90-473a-8f6d-b156e4f98d8f"
+groq_api_keys="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"
+astra_db_app_token= "AstraCS:rCKjqBeLoCSOUBgiPNMSrTyp:8b4d5ac5e078e595b141cbd3886fdd0d158421abc9a95c144d1b615171ad875b"
+astra_db_id="a0d8a73e-6cc0-439d-a8c9-e511d8f2c3cc"
 cassio.init(token=astra_db_app_token,database_id=astra_db_id)
 
 
@@ -49,3 +49,31 @@ astra_vector_store.add_documents(docs)
 print("Inserted %i headlines." % len(docs))
 
 astra_vector_index = VectorStoreIndexWrapper(vectorstore=astra_vector_store)
+
+
+#groq and chat template
+llm=ChatGroq(groq_api_key=groq_api_keys,
+         model_name="mixtral-8x7b-32768")
+
+from langchain_core.prompts import ChatPromptTemplate
+prompt = ChatPromptTemplate.from_template("""
+Answer the following question based only on the provided context. 
+Think step by step before providing a detailed answer. 
+I will tip you $1000 if the user finds the answer helpful. 
+<context>
+{context}
+</context>
+
+Question: {input}""")
+
+
+
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+retriever=astra_vector_store.as_retriever()
+document_chain=create_stuff_documents_chain(llm,prompt)
+retrieval_chain=create_retrieval_chain(retriever,document_chain)
+
+response=retrieval_chain.invoke("Explain the self-reflection")
+print(response['answer'])
